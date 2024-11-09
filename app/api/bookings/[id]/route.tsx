e4634b64bf934a804bc45d4f08c748b9358453cf
Daniel Bariche <dabar@kth.se>
2024-11-09 16:28:30 +0100

    PUT endpoint for booking room

--- app/api/bookings/[id]/route.tsx
+++ app/api/bookings/[id]/route.tsx
@@ -16,3 +16,34 @@ export async function GET(
   }
   return NextResponse.json(booking);
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  const { id } = await params;
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const booking = await prisma.booking.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!booking)
+    return NextResponse.json({ error: "Booking not found" }, { status: 404 });
+
+  const updatedBooking = await prisma.booking.update({
+    where: { id: booking.id },
+    data: {
+      date: body.date,
+      startTime: body.startTime,
+      endTime: body.endTime,
+      bookedBy: body.bookedBy,
+      roomId: body.roomId,
+    },
+  });
+  return NextResponse.json(updatedBooking);
+}
