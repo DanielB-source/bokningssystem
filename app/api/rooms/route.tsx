c0fe28ab40ff09adf5914c277fa790a4183cc046
Daniel Bariche <dabar@kth.se>
2024-11-09 10:52:28 +0100

    Create room

--- app/api/rooms/route.tsx
+++ app/api/rooms/route.tsx
@@ -1,7 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import schema from "./schema";
 
 export async function GET(request: NextRequest) {
   const rooms = await prisma.room.findMany();
   return NextResponse.json(rooms);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const room = await prisma.room.create({
+    data: {
+      name: body.name,
+      capacity: body.capacity,
+    },
+  });
+  return NextResponse.json(room, { status: 201 });
 }
