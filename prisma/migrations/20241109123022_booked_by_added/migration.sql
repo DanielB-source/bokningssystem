a855c2570732f49a02ee6818bd9b9f8d3a6095b3
Daniel Bariche <dabar@kth.se>
2024-11-09 13:31:32 +0100

    add 'bookedBy' field to Booking model

---
+++ prisma/migrations/20241109123022_booked_by_added/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `bookedBy` to the `Booking` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Booking" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" DATETIME NOT NULL,
+    "startTime" DATETIME NOT NULL,
+    "endTime" DATETIME NOT NULL,
+    "bookedBy" TEXT NOT NULL,
+    "roomId" INTEGER NOT NULL,
+    CONSTRAINT "Booking_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Booking" ("date", "endTime", "id", "roomId", "startTime") SELECT "date", "endTime", "id", "roomId", "startTime" FROM "Booking";
+DROP TABLE "Booking";
+ALTER TABLE "new_Booking" RENAME TO "Booking";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
