30ffdcb4b6ef1381c19714b0efbca71106c1d499
Daniel Bariche <dabar@kth.se>
2024-11-09 11:12:26 +0100

    add duplicate room check

--- app/api/rooms/route.tsx
+++ app/api/rooms/route.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import schema from "./schema";
+import { error } from "console";
 
 export async function GET(request: NextRequest) {
   const rooms = await prisma.room.findMany();
@@ -13,11 +14,18 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const room = await prisma.room.create({
+  const room = await prisma.room.findUnique({
+    where: { name: body.name },
+  });
+
+  if (room)
+    return NextResponse.json({ error: "Room already exists" }, { status: 400 });
+
+  const newRoom = await prisma.room.create({
     data: {
       name: body.name,
       capacity: body.capacity,
     },
   });
-  return NextResponse.json(room, { status: 201 });
+  return NextResponse.json(newRoom, { status: 201 });
 }
