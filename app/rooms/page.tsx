7aa4da33eb9162efb6474c6d110a7637fc46365f
Daniel Bariche <dabar@kth.se>
2024-11-10 16:32:20 +0100

    Initial setup with basic room fetching

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,7 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
+
+interface Booking {
+  id: number;
+  name: string;
+  capacity: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+  bookedBy: string | null;
+}
+
 const Rooms = () => {
+  const [bookings, setBookings] = useState<Booking[]>([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/bookings");
+        const data: Booking[] = await response.json();
+        setBookings(data);
+      } catch (error) {
+        console.error("Error fetching bookings:", error);
+      }
+    };
+    fetchBookings();
+  }, []);
+
   return (
-    <div>
-      <h1>Välj en tid</h1>
+    <div className="p-10">
+      <h1 className="text-2xl font-bold mb-4">Välj ett rum</h1>
+      {bookings.map((booking) => (
+        <div key={booking.id}>
+          {booking.name}
+          {booking.capacity}
+          {booking.date}
+          {booking.startTime}
+          {booking.endTime}
+        </div>
+      ))}
     </div>
   );
 };
