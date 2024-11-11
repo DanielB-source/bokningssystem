088a00055754647f9b61868c498a502773c9ce6a
Daniel Bariche <dabar@kth.se>
2024-11-11 07:35:05 +0100

    Adjust homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
-// import { generateRooms } from "./roomGenerator";
 
 export default function Home() {
-  // generateRooms();
   return (
-    <div className="flex h-screen flex-col justify-between items-center ">
-      <h1 className="text-9xl">Boka ett rum</h1>
-      <Link href="/rooms" className="btn w-96 h-20 btn-neutral mb-10">
-        Boka
-      </Link>
+    <div className="flex h-screen flex-col justify-between items-center px-4">
+      <div className="flex flex-col items-center mt-20">
+        <h1 className="text-6xl md:text-7xl lg:text-9xl text-center">
+          Boka ett rum
+        </h1>
+      </div>
+      <div className="w-full flex justify-center mb-10">
+        <Link
+          href="/rooms"
+          className="btn w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-16 sm:h-20 btn-neutral rounded-lg"
+        >
+          Boka
+        </Link>
+      </div>
     </div>
   );
 }
